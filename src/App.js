0a35e257c2c8721c1c3fb411ca6276ca0980d231
JeffQuin <quinonezaguirrejeffersongervac@gmail.com>
2024-04-06 19:23:02 -0400

    Presentación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Testimonio from './componentes/Testimonio.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos</h1>
+        <Testimonio
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='1'
+          cargo='Ingenieria de software'
+          empresa='Spotify'
+          testimonio='Mm, rite of passage, classic maverick Match in the gas tank, ooh, thats wretched Unstoppable, legendary animal, mm Digital justice, now you re gonna know us Hail to the king and queen of the ruckus Yacht Money wired, no denying ' />
+          <Testimonio
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='2'
+          cargo='Ingenieria de software'
+          empresa='Amazon'
+          testimonio='Báilame como si fuera la última vez Y enséñame ese pasito que no sé Un besito bien suavecito, bebé  Taki taki Taki taki, rumba' />
+          <Testimonio
+          nombre='Sarah chima'
+          pais='Nigeria'
+          imagen='3'
+          cargo='Ingenieria de software'
+          empresa='ChatDesk'
+          testimonio='Mordiendo mis labios verás Que nadie más está en mi camino Nada tiene por qué importar Déjalo atrás, estás conmigo Mordiendo mis labios verás Que nadie más está en mi camino Nada tiene por qué importar Déjalo atrás, estás conmigo' />
+      </div>
     </div>
   );
 }
